070d4b45005b0aa7f5df020d4a37c9c9874e66b4
10xJosh <55113224+10xJosh@users.noreply.github.com>
2023-01-24 14:21:26 -0700

    Add project files.

---
+++ JoshsFinancialplanner/DataBase/dbo.Table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [January] NCHAR(10) NOT NULL, 
+    [February] NCHAR(10) NOT NULL, 
+    [March] NCHAR(10) NOT NULL, 
+    [April] NCHAR(10) NOT NULL, 
+    [May] NCHAR(10) NOT NULL, 
+    [June] NCHAR(10) NOT NULL, 
+    [July] NCHAR(10) NOT NULL, 
+    [August] NCHAR(10) NOT NULL, 
+    [September] NCHAR(10) NOT NULL, 
+    [October] NCHAR(10) NOT NULL, 
+    [November] NCHAR(10) NOT NULL, 
+    [December] NCHAR(10) NOT NULL
+)
---
+++ JoshsFinancialplanner/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JoshsFinancialplanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFilter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
